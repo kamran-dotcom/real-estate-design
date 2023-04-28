e9fc434fc14513ad0c133193565c3865f0c6cef4
kamran-dotcom <kamichannar2@gmail.com>
2023-04-28 17:40:43 +0500

    header and hero done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Companies } from "./components/Companies/Companies";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Hero/>
+      <Companies/>
+    </div>
+  );
+}
+
+export default App;
